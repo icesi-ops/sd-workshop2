3d17a01e60999611588f5680a1c64a9aace14934
Anderson cardenas <andyguaca@gmail.com>
2023-03-01 18:22:27 -0500

    workshop2(complete)

---
+++ scripts/glusterfs.sh
@@ -0,0 +1,10 @@
+yum install -y centos-release-gluster
+yum install -y glusterfs-server
+yum install -y xfsprogs
+service glusterd start
+sfdisk /dev/sdb << EOF
+;
+EOF
+mkfs.xfs /dev/sdb1
+mkdir -p /gluster/data 
+mount /dev/sdb1 /gluster/data/
