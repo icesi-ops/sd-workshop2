acf634b623e5e89f772b52eb0a40174035241529
Esteban Yusunguaira López <zonitidus@gmail.com>
2023-03-02 23:30:48 -0500

    fix: ssh commands format

--- scripts/nodes_gluster.sh
+++ scripts/nodes_gluster.sh
@@ -6,8 +6,8 @@ echo $MASTERHOST
 
 # Check if "master" machine is running
 if [ "$(vagrant status | grep 'node3' | awk '{print $2}')" == "running" ]; then
-    vagrant ssh node1 -- -t sudo mount.glusterfs localhost:/gv0 /mnt; exit;
-    vagrant ssh node2 -- -t sudo mount.glusterfs localhost:/gv0 /mnt; exit;
+    vagrant ssh node1 -- -t "sudo mount.glusterfs localhost:/gv0 /mnt;exit"
+    vagrant ssh node2 -- -t "sudo mount.glusterfs localhost:/gv0 /mnt;exit"
 else
     echo "Master machine is not running."
 fi
